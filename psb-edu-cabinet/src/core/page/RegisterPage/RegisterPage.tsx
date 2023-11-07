6ca5bc1502cfd2d5fcda9b6ea9d70051fcded630
arteria <denis.rjabko@mail.ru>
2023-11-07 21:43:51 +0300

    add register-page

--- psb-edu-cabinet/src/core/page/RegisterPage/RegisterPage.tsx
+++ psb-edu-cabinet/src/core/page/RegisterPage/RegisterPage.tsx
@@ -1,9 +1,73 @@
-import React, { FC } from "react"
-import "./RegisterPage.scss"
+import { Button, Card, Div, FormItem, FormLayout, Input, Title } from "@vkontakte/vkui";
+import React, { FC } from "react";
+import { UserFullInfo, useLoginMutation } from "../../store/api/auth-api";
+import "./RegisterPage.scss";
 
 const RegisterPage: FC = () => {
+    const [email, setEmail] = React.useState('');
+    const [password, setPassword] = React.useState('');
+    const [name, setName] = React.useState('');
+
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const [register, { isLoading: isLoadingLogin, isSuccess: isSuccessLogin, isError: isErrorLoading, error: errorLogin }] = useLoginMutation();
+    function onLoginClick() {
+        const body: UserFullInfo = {
+            email, password, name
+        }
+        register(body)
+    }
     return (<div className="register-page-container">
-        RegisterPage
+        <Card className="center-message" mode="outline-tint">
+            <Div className="login-modal" >
+                <Title className="title" level="1" >
+                    Регистрация
+                </Title>
+
+                <FormLayout>
+                    <FormItem top="ФИО" htmlFor="pass" className="form" status={name ? 'valid' : 'error'}
+                    >
+                        <Input id="pass" type="password" placeholder="Фамилия Имя Отчество" value={name}
+                            onChange={(event) => setName(event.currentTarget.value)}
+
+                        />
+                    </FormItem>
+
+                    <FormItem className="form"
+                        htmlFor="email"
+                        top="Логин"
+                        status={email ? 'valid' : 'error'}
+                        bottomId="email-type"
+                    >
+                        <Input
+                            aria-labelledby="email-type"
+                            id="email"
+                            type="email"
+                            placeholder="Введите email"
+                            name="email"
+                            value={email}
+                            onChange={(event) => setEmail(event.currentTarget.value)}
+                        />
+                    </FormItem>
+                    <FormItem top="Пароль" htmlFor="pass" className="form" status={password ? 'valid' : 'error'}
+                    >
+                        <Input id="pass" type="password" placeholder="Введите пароль" value={password}
+                            onChange={(event) => setPassword(event.currentTarget.value)}
+
+                        />
+                    </FormItem>
+                </FormLayout>
+                <Button
+                    appearance="accent-invariable"
+                    size="l"
+                    mode="primary"
+
+                    disabled={email.length == 0 || password.length == 0}
+                    onClick={() => onLoginClick()}
+                >
+                    Зарегестрироваться
+                </Button>
+            </Div>
+        </Card>
     </div>)
 }
 
--- psb-edu-cabinet/src/core/store/api/auth-api.ts
+++ psb-edu-cabinet/src/core/store/api/auth-api.ts
@@ -3,15 +3,18 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 import environment from '../../../enviroment'
 import { Application } from '../../../types/application'
 // Define a service using a base URL and expected endpoints
-export type UserInfo = {
+export interface UserInfo {
     email: string,
     password: string
+}
+export interface UserFullInfo extends UserInfo {
+    name: string
 }
 export const authApi = createApi({
     reducerPath: 'applicationsApi',
     baseQuery: fetchBaseQuery({ baseUrl: environment.urlBackend }),
     endpoints: (builder) => ({
-        login: builder.mutation<Application[], UserInfo>({
+        login: builder.mutation<null, UserInfo>({
             query: (userInfo: UserInfo) => (
                 {
                     url: "Auth/login",
@@ -21,10 +24,19 @@ export const authApi = createApi({
             ),
 
         }),
+        register: builder.mutation<null, UserFullInfo>({
+            query: (userInfo: UserInfo) => (
+                {
+                    url: "Auth/register",
+                    method: "POST",
+                    body: userInfo
+                }
+            ),
 
+        }),
     }),
 })
 
 // Export hooks for usage in functional components, which are
 // auto-generated based on the defined endpoints
-export const { useLoginMutation } = authApi
+export const { useLoginMutation, useRegisterMutation } = authApi
